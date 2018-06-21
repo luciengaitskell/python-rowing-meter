fa2f2bc4cc23181c6d58fc0ca970814a7eacb016
Lucien Gaitskell <lucien@gaitskell.com>
2018-06-21 13:55:08 -0400

    Add device: 'HUZZAH32/ESP32 Battery Monitor'

---
+++ rowing/devices/esp32_batv.py
@@ -0,0 +1,28 @@
+"""
+Easy implementation for ESP32 Battery voltage monitor
+
+Written by Lucien Gaitskell, 2018
+"""
+from machine import ADC, Pin
+
+
+SCALE = (3.5 / 4095) * 2  # adc scale * voltage divider
+
+
+class BatteryVoltage:
+    def __init__(self):
+        # Setup ADC:
+        self.batv = ADC(Pin(35))  # Analog A13 -- battery monitor
+
+        # Set attenuation and bit width:
+        self.batv.atten(ADC.ATTN_11DB)
+        self.batv.width(ADC.WIDTH_12BIT)
+
+    def read(self):
+        return self.batv.read()
+
+    def get_volt(self, val):
+        return val*SCALE
+
+    def read_volt(self):
+        return self.get_volt(self.read())
